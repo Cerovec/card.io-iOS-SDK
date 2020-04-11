561e418f462921b13875a3436fce66629d23f5fd
Jurica Cerovec <jurica.cerovec@microblink.com>
2020-04-11 20:04:05 +0200

    Updated swift sample to work with the latest Xcode

--- SampleApp-Swift/SampleApp-Swift/ViewController.swift
+++ SampleApp-Swift/SampleApp-Swift/ViewController.swift
@@ -9,34 +9,43 @@ import UIKit
 
 class ViewController: UIViewController, CardIOPaymentViewControllerDelegate {
 
-  @IBOutlet weak var resultLabel: UILabel!
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    CardIOUtilities.preload()
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  @IBAction func scanCard(sender: AnyObject) {
-    let cardIOVC = CardIOPaymentViewController(paymentDelegate: self)
-    cardIOVC.modalPresentationStyle = .FormSheet
-    presentViewController(cardIOVC, animated: true, completion: nil)
-  }
-  
-  func userDidCancelPaymentViewController(paymentViewController: CardIOPaymentViewController!) {
-    resultLabel.text = "user canceled"
-    paymentViewController?.dismissViewControllerAnimated(true, completion: nil)
-  }
-  
-  func userDidProvideCreditCardInfo(cardInfo: CardIOCreditCardInfo!, inPaymentViewController paymentViewController: CardIOPaymentViewController!) {
-    if let info = cardInfo {
-      let str = NSString(format: "Received card info.\n Number: %@\n expiry: %02lu/%lu\n cvv: %@.", info.redactedCardNumber, info.expiryMonth, info.expiryYear, info.cvv)
-      resultLabel.text = str as String
+    func userDidCancel(_ paymentViewController: CardIOPaymentViewController!) {
+        resultLabel.text = "user canceled"
+        paymentViewController?.dismiss(animated: true, completion: nil)
     }
-    paymentViewController?.dismissViewControllerAnimated(true, completion: nil)
-  }  
+
+    func userDidProvide(_ cardInfo: CardIOCreditCardInfo!, in paymentViewController: CardIOPaymentViewController!) {
+        if let info = cardInfo {
+          let str = NSString(format: "Received card info.\n Number: %@\n expiry: %02lu/%lu\n cvv: %@.", info.redactedCardNumber, info.expiryMonth, info.expiryYear, info.cvv)
+          resultLabel.text = str as String
+        }
+        paymentViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    @IBOutlet weak var resultLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        CardIOUtilities.preload()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func scanCard(sender: AnyObject) {
+        let cardIOVC = CardIOPaymentViewController(paymentDelegate: self)
+
+        guard let cvc = cardIOVC else {
+            print("Failed initialization of CardIO")
+            return
+        }
+
+        cvc.modalPresentationStyle = .formSheet
+        present(cvc, animated: true, completion: nil)
+    }
+
 }
